36f2d0900d5b5bd9f16a00a28a3bf78a98c1277c
jbonnet <jerome.bonnet@imerir.com>
2017-11-06 15:05:03 +0100

    osef

--- db.sql
+++ db.sql
@@ -1,5 +1,6 @@
-DROP TABLE IF EXISTS User;
-
-CREATE TABLE User(
+DROP TABLE IF EXISTS inscrit CASCADE;
+CREATE TABLE inscrit(
 	id_joueur varchar(50) PRIMARY KEY
 	);
+	
+INSERT INTO inscrit(id_joueur) VALUES ('un chat la');
