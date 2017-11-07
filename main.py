a6c0d2033eda50f61e02a5557eae2799bb36bdc4
jbonnet <jerome.bonnet@imerir.com>
2017-11-07 17:44:53 +0100

    chala

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def ajout_inscrit():
         print(json.dumps(data))
         print(data['id_joueur'])
         #db.execute("INSERT INTO inscrit(id_joueur) VALUES ('%s');",(data['id_joueur']))
-        db.execute("INSERT INTO inscrit(id_joueur) VALUES ('%s');",('tata'))
+        db.execute("INSERT INTO inscrit(id_joueur) VALUES (id_joueur);",('tata'))
         db.close()
         # Je récupére l'id du dernier joueur ajouté
         return json.dumps('OK'), 201, {'Content-Type': 'application/json'}
